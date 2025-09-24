b5840db357a52c2f33fb13a01e3143f6b06ba284
RumpleSteelSkin <osmnistbayrak@gmail.com>
2025-09-25 00:05:12 +0300

    Created APIGateway project and configured services with Ocelot; added Docker Compose and verified with tests

---
+++ ESourcing.APIGateway/Program.cs
@@ -0,0 +1,9 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddOcelot(new ConfigurationBuilder().AddJsonFile("ocelot.json").Build());
+
+var app = builder.Build();
+await app.UseOcelot();
+await app.RunAsync();
